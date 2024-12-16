7e9be1685fc9d8b6b3ff34d5b6d2bb0da0080caf
CCR Astra <95889406+CCRAstra-official@users.noreply.github.com>
2024-12-15 19:20:24 -0600

    Update +page.svelte

--- src/routes/minecraft/+page.svelte
+++ src/routes/minecraft/+page.svelte
@@ -4,7 +4,8 @@
 
 <div class="mx-6 mt-12 flex flex-col items-center justify-center">
 	<h1 class="oswald-font text-4xl font-black">Minecraft Server</h1>
-	<p class="mt-2 text-xl text-gray-400">For now you can join our Minecraft Server via applying in the discord server! More details soon...</p>
+	<p class="mt-2 text-xl text-gray-400">CCR hosts a <u>Public</u> Minceraft: Bedrock Edition realm! This realms includes a series of addons, such as the Better on Bedrock mod. With each "season", the mods and features of the realm change.
+The realm was created by only two people! CCR Astra and CCR Azrael. You can join the realm with this link: <a href="https://realms.gg/vyXXQjRz__A7_KA">realms.gg/vyXXQjRz__A7_KA</a>. Addon Ons list coming soon, Realm Link Button coming soon.</p>
 </div>
 
 <style>
