f02b7ae25255145e2c781fac3be74be22d3f8540
CCR Astra <95889406+CCRAstra-official@users.noreply.github.com>
2024-12-16 21:26:13 -0600

    Update About Me Information

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,9 @@
 <div class="mx-6 mt-12 flex flex-col items-center justify-center">
 	<h1 class="oswald-font text-4xl font-black">Who are we?</h1>
 	<p class="mt-2 text-xl text-gray-400">
-		This is an Invite Only "Clan" that partakes in videogames.   We have a series of games we take part in.
+		CCR is an application only closed Clan. CCR is not only a clan, but a community for those to interact, play games, and have fun. While we encorage members to climb the ranks, we have
+plenty of casual gamers and even some people who don't game at all.  Our name has been seen across <u><a href="https://discord.com">Discord</a></u> for a while, maybe you have seen it? We play games such as 
+Fortnite, Minecraft, Overwatch 2, Marvel Rivals, and other great games. CCR members are forever strong as one, but they can build their path together.  What does CCR stand for? I guess you'll never know.
 	</p>
 </div>
 
