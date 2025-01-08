6fc3fed77430aac7d52f28a5d6e920eb2af55c8a
CCR Astra <95889406+CCRAstra-official@users.noreply.github.com>
2025-01-07 23:27:17 -0600

    Update +page.svelte

--- src/routes/SRC/+page.svelte
+++ src/routes/SRC/+page.svelte
@@ -1,1 +1,13 @@
+<div class="mx-6 mt-12 flex flex-col items-center justify-center">
+	<h1 class="oswald-font text-4xl font-black">Silent Reaper Clan</h1>
+	<p class="mt-2 text-xl text-gray-400"This page and it's details coming soon.</p>
+</div>
 
+<style>
+	.oswald-font {
+		font-family: 'Oswald', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 700;
+		font-style: bold;
+	}
+</style>
