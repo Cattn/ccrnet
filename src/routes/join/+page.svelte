43b41deb861f42ec5a8f2b4e288609a8762897c6
CCR Astra <95889406+CCRAstra-official@users.noreply.github.com>
2025-04-30 19:31:02 -0500

    Fix show no new members
    My fingers must be fat or something

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="mx-auto mt-8 flex w-full max-w-7xl flex-col items-center justify-center px-4 sm:px-6">
 	<h1 class="oswald-font text-2xl font-black underline sm:text-4xl">How to Join</h1>
-<p class="mt-4 text-center text-base text-gray-400 sm:text-lg"We are not accepting any new members at this time.</p>
+<p class="mt-4 text-center text-base text-gray-400 sm:text-lg"We are not accepting any new members at this time."</p>
 	<Button
 		href="https://discord.gg/5DqAzjSh9C"
 		variant="secondary"
